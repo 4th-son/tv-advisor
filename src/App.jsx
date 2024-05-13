235a4cb1860bc6c5b22ae9651053f3537927bcb4
Michael ogozi <michealogozi@gmail.com>
2024-05-13 23:23:37 +0300

    first commit setting up the environment

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import LandingPage from "./assets/components/landingPage";
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/components/landingPage.jsx
@@ -0,0 +1,10 @@
+function landingPage() {
+  return (
+    <div className=" h-screen w-screen bg-black text-white font-extrabold">
+      {" "}
+      hello ladies{" "}
+    </div>
+  );
+}
+
+export default landingPage;
