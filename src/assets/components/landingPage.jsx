b850e7aa32146fc41653ddd94bbf651b2f8e61f8
Michael ogozi <michealogozi@gmail.com>
2024-06-19 22:43:16 +0300

    building the layout

--- src/assets/components/landingPage.jsx
+++ src/assets/components/landingPage.jsx
@@ -1,8 +1,9 @@
 function landingPage() {
   return (
-    <div className=" h-screen w-screen bg-black text-white font-extrabold">
-      {" "}
-      hello ladies{" "}
+    <div className=" main_container h-screen flex flex-col p-6 bg-black text-white font-extrabold">
+      <div className=" header "></div>
+      <div className=" tvShow_detail"></div>
+      <div className=" recommended_shows"></div>
     </div>
   );
 }
